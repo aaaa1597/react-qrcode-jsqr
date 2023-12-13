9bc31d0ed26c54d3dd2981c130371098c42c8131
aaaa1597 <tokushi2011@gmail.com>
2023-12-13 21:12:30 +0900

    完成

--- src/components/QRCodeScanner.tsx
+++ src/components/QRCodeScanner.tsx
@@ -30,25 +30,61 @@ const QRCodeScanner = () => {
     const openCamera = async () => {
       const video = videoRef.current;
       if (video) {
+        video.addEventListener('play', (event) => { console.log(event); console.log("(w,h)=(", video.videoWidth, ", " + video.videoHeight, ")") })
         const stream = await navigator.mediaDevices.getUserMedia(constraints);
         video.srcObject = stream;
-
-        navigator.mediaDevices.enumerateDevices()
-        .then(function(devices) {
-          devices.forEach(function(device) {
-              if(device.kind === "videoinput"){
-                  console.log(device.kind + ": " + device.label + ":" + device.deviceId);
-                  // ここでdeviceのリストを作って利用するなど
-              }
-          });
-        })
-        .catch(function(err) {
-          console.log(err.name + ": " + err.message);
-        });
       }
     };
     openCamera();
   }, [])
+
+  useEffect(() => {
+    if (!isContinue) {
+      return;
+    }
+
+    const decodeQRCode = () => {
+      const context = canvasRef?.current?.getContext('2d');
+      const video = videoRef?.current;
+
+      if (!context || !video) {
+        return;
+      }
+
+      context.beginPath();
+      context.fillStyle = 'rgb( 0, 0, 0)';
+      context.fillRect(0, 0, 100, 200);
+
+      context.drawImage(video, 0, 0, videoWidth, videoHeight);
+      const imageData = context.getImageData(0, 0, videoWidth, videoHeight);
+      const code = jsQR(imageData.data, videoWidth, videoHeight);
+
+      return code?.data;
+    };
+
+    const intervalId = window.setInterval(() => {
+      const decodedValue = decodeQRCode();
+
+      if (!decodedValue || qrCodeData.includes(decodedValue)) {
+        return;
+      }
+
+      setQrCodeData([...qrCodeData, decodedValue]);
+    }, 1_000 / videoFrameRate);
+    intervalRef.current = intervalId;
+
+    return () => {
+      clearInterval(intervalRef.current);
+    };
+  }, [isContinue, qrCodeData]);
+
+  const handleStart = () => {
+    setIsContinue(true);
+  };
+
+  const handleStop = () => {
+    setIsContinue(false);
+  };
 
   return (
     <div className="App">
@@ -63,8 +99,8 @@ const QRCodeScanner = () => {
           <p>{qrCodeData.join('\n')}</p>
         </div>
         <div>
-          {/* <button onClick={handleStart}>Start Scan</button>
-          <button onClick={handleStop}>Stop Scan</button> */}
+          <button onClick={handleStart}>Start Scan</button>
+          <button onClick={handleStop}>Stop Scan</button>
         </div>
       </div>
       hello QRCodeScanner
